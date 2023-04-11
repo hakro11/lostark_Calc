8dea5c1bb7557b1c0fbf21e68cf0775c9f90e4fe
hakro <leehr125@naver.com>
2023-04-11 23:24:47 +0900

    update project

--- src/interface/interface.ts
+++ src/interface/interface.ts
@@ -180,3 +180,25 @@ export interface Engraving {
     EngravingEffect: [number, number, number, number]
 
 }
+
+export interface userInfo {
+    rootClass : "Warrior" | "Fighter" | "Hunter" | "Magician" | "Assassin" | "Specialist"
+    className: string
+    engraving: string[]
+    specializationRatio?: number
+    ultiRatio?: 0.054648648648649
+    useSkillInfo?: SkillInfo[]
+}
+
+export interface SkillInfo {
+    skillName: string
+    skillType : string
+    skillBasic: number
+    skillRatio: number
+}
+
+interface UseCards{
+    cardName : string | null
+    cardIcon : string | null
+}
+
---
+++ src/utils/damCalc.ts
@@ -0,0 +1,12 @@
+import { userInfo } from '../interface/interface';
+import { classInformation } from './../constants/classInfo';
+
+
+export function damCalc(root_Class: userInfo[][]) {
+    let attackDam
+    const damage = attackDam
+}
+
+function catchClass(userCharInfo: any, classInformation: userInfo[]) {
+    classInformation.filter(i => i.className == "")
+}
