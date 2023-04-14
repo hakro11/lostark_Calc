f35c953b9a51e11eca56e0c0f9a189afc6869c47
hakro <leehr125@naver.com>
2023-04-14 13:03:38 +0900

    user class와 classInfo 데이터 취출 및 user 기본공격력 파싱

--- src/utils/damCalc.ts
+++ src/utils/damCalc.ts
@@ -1,12 +1,27 @@
-import { userInfo } from '../interface/interface';
 import { classInformation } from './../constants/classInfo';
 
+export default function damCalc(props: any) {
+    const charInfo = props.charInfo;
 
-export function damCalc(root_Class: userInfo[][]) {
-    let attackDam
-    const damage = attackDam
+    let userSkill = loadClassSkillInfo();
+    let attackDam = catchAttackDam();
+
+    // const damage = attackDam
+
+    function loadClassSkillInfo() {
+        const classSkillInfo = classInformation
+            .filter(i => i.className === charInfo.ArmoryProfile.CharacterClassName);
+
+        return classSkillInfo;
+    }
+
+    function catchAttackDam() {
+        let charAttackDam = charInfo.ArmoryProfile.Stats[7]
+            .Tooltip[1].match(/[>][0-9]{1,6}[</font$]/);
+        charAttackDam = charAttackDam[0].substring(1, charAttackDam[0].length - 1);
+
+        return charAttackDam;
+    }
 }
 
-function catchClass(userCharInfo: any, classInformation: userInfo[]) {
-    classInformation.filter(i => i.className == "")
-}
+
